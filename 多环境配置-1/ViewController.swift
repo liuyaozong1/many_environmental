101b440bb06de936bb0ec46b3461ece25d105953
liuyaozong1 <648731281@qq.com>
2022-02-10 10:22:56 +0800

    项目上传

--- 多环境配置-1/ViewController.swift
+++ 多环境配置-1/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+        let path = Bundle.main.path(forResource: "Info", ofType: "plist")!
+        let infoDic = NSDictionary(contentsOfFile: path)!
+        print("\(infoDic["HOST_URL"]!)")
+        
+        #if TEST
+        print("测试测试测试")
+        #endif
+        
+    
+        
     }
 
 
